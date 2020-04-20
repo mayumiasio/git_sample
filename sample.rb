80cbd0b98f7f7dadccc07f4038b84be0428fa8ce
mayumiasio <mayumayumayumi.11@gmail.com>
2020-04-20 16:54:05 +0900

    Update sample.rb

--- sample.rb
+++ sample.rb
@@ -5,11 +5,16 @@
 # def hello
 #   p "hello, Git."
 # end
+# def hello
+#   p "hello, GitHub."
+# end
+
+# hello
+
 def hello
-  p "hello, GitHub."
+  p "hello, GitHub Desktop."
 end
 
 hello
-
 
 hello
